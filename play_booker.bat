2aa780a0fcf48f0281b036fa7992ac8e058211e1
redsett <ko1o33@hotmail.com>
2024-08-15 21:34:02 -0700

    About to update to 4,3

--- play_booker.bat
+++ play_booker.bat
@@ -1,3 +1,3 @@
 setlocal
 cd /d %~dp0
-"D:\Personal\Godot\Godot_v4.3-rc3_win64.exe\Godot_v4.3-rc3_win64.exe" --path ../../Booker
+"D:\Personal\Godot\Godot_v4.3-stable_win64.exe\Godot_v4.3-stable_win64.exe" --path ../../Booker
