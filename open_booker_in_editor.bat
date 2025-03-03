0a8128ce4c8e118607ec8ad02fc61a16e359011c
redsett <ko1o33@hotmail.com>
2025-03-03 11:17:33 -0800

    Updated to 4.4 stable.

--- open_booker_in_editor.bat
+++ open_booker_in_editor.bat
@@ -1,4 +1,4 @@
 setlocal
 cd /d %~dp0
 python ../delete_dll.py
-"D:\Personal\Godot\Godot_v4.4-dev5_win64.exe\Godot_v4.4-dev5_win64.exe" --path ../../Booker --editor
+"D:\Personal\Godot\Godot_v4.4-stable_win64.exe\Godot_v4.4-stable_win64.exe" --path ../../Booker --editor
