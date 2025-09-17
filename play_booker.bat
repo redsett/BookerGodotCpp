0e52f1f3ed5c7a1c647f2167b1905de575aab0a8
redsett <ko1o33@hotmail.com>
2025-09-16 22:02:22 -0700

    Upgraded to 4.5.

--- play_booker.bat
+++ play_booker.bat
@@ -1,3 +1,3 @@
 setlocal
 cd /d %~dp0
-"D:\Personal\Godot\Godot_v4.4-stable_win64.exe\Godot_v4.4-stable_win64.exe" --path ../../Booker
+"D:\Personal\Godot\Godot_v4.5-stable_win64.exe\Godot_v4.5-stable_win64.exe" --path ../../Booker
